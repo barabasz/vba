a921d553f8c956b3d8c27bb64637d2bb65702a75
Andrew <abarabasz@gmail.com>
2024-04-02 15:00:38 +0200

    Create TextFromCell.vb

---
+++ TextFromCell.vb
@@ -0,0 +1,8 @@
+Private Function TextFromCell(tableId As Integer, row As Integer, column As Integer) As String
+    Dim cell As Range
+    Dim cellEndChars As String
+    cellEndChars = Chr(7) & Chr(13)
+    Set cell = ActiveDocument.Tables(tableId).cell(row, column).Range
+    cell.MoveEndWhile Cset:=cellEndChars, Count:=wdBackward
+    TextFromCell = cell.Text
+End Function
